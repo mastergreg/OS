53261d9cac1b6308bd2145f4356b056cd15c5385
mastergreg <gregliras@gmail.com>
2011-11-10 14:09:21 +0200

    added comments on top

--- ask_1/fconc.c
+++ ask_1/fconc.c
@@ -1,3 +1,19 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : fconc.c
+
+* Purpose :
+
+* Creation Date : 20-12-2008
+
+* Last Modified : Thu 10 Nov 2011 02:07:34 PM EET
+
+* Created By : Greg Liras <gregliras@gmail.com>
+ 
+* Created By : Vasilis Gerakaris <vgerak@gmail.com>
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+
 #include <stdio.h>
 #include <fcntl.h>
 
@@ -5,44 +21,44 @@
 
 int main(int argc, char ** argv)
 {
-   FILE *A;
-   FILE *B;
-   FILE *OUT;
-   if (argc < 3)
-   {
-      printf("Usage: ./fconc infile1 infile2 [outfile (default:fconc.out)]\n");
-      return -1;
-   }
-   A = fopen(argv[1],"r");
-   if (A < 0)
-   {
-      printf("%s: No such file or directory\n",argv[1]);
-      return -1;
-   }
-   B = fopen(argv[2],"r");
-   if (B < 0)
-   {
-      printf("%s: No such file or directory\n",argv[2]);
-      return -1;
-   }
-   if (argc > 3)
-   {
-      OUT = fopen(argv[3],"w");
-   }
-   else
-   {
-      OUT = fopen("fconc.out","w");
-   }
-   if (OUT < 0)
-   {
-      printf("Error handling output file\n");
-      return -1;
-   }
+  FILE *A;
+  FILE *B;
+  FILE *OUT;
+  if (argc < 3)
+  {
+    printf("Usage: ./fconc infile1 infile2 [outfile (default:fconc.out)]\n");
+    return -1;
+  }
+  A = fopen(argv[1],"r");
+  if (A < 0)
+  {
+    printf("%s: No such file or directory\n",argv[1]);
+    return -1;
+  }
+  B = fopen(argv[2],"r");
+  if (B < 0)
+  {
+    printf("%s: No such file or directory\n",argv[2]);
+    return -1;
+  }
+  if (argc > 3)
+  {
+    OUT = fopen(argv[3],"w");
+  }
+  else
+  {
+    OUT = fopen("fconc.out","w");
+  }
+  if (OUT < 0)
+  {
+    printf("Error handling output file\n");
+    return -1;
+  }
 
-   doWrite(
+  doWrite();
 
 
 
-         return 0;
-         }
+  return 0;
+}
 
--- ask_1/fconc.h
+++ ask_1/fconc.h
@@ -1,3 +1,19 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : fconc.h
+
+* Purpose :
+
+* Creation Date : 20-12-2008
+
+* Last Modified : Thu 10 Nov 2011 02:09:06 PM EET
+
+* Created By : Greg Liras <gregliras@gmail.com>
+
+* Created By : Vasilis Gerakaris <vgerak@gmail.com>
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+
 void doWrite(int fd, const char *buff, int len);
 void write_file(int fd, const char *infile);
 
