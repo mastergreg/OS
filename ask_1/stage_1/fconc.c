80fa80667dd2a99c10ba4b9206eeb5f3581960f7
mastergreg <gregliras@gmail.com>
2011-11-24 15:37:53 +0200

    updated lock mechanism

--- ask_1/stage_1/fconc.c
+++ ask_1/stage_1/fconc.c
@@ -2,7 +2,7 @@
 
  * File Name : fconc.c
 
- * Last Modified : Wed 23 Nov 2011 02:15:21 AM EET
+ * Last Modified : Thu 24 Nov 2011 03:36:34 PM EET
 
  * Created By : Greg Liras <gregliras@gmail.com>
 
@@ -14,151 +14,175 @@
 
 int main(int argc, char ** argv)
 {
-  int OUT;
-  int TMP;
-  int i;
-  const char * output;
-  int duplicate = 0;
-  int W_FLAGS = O_CREAT | O_WRONLY | O_TRUNC;
-  int C_PERMS = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH ;
-  struct flock lock;
+    int OUT;
+    int TMP;
+    int i;
+    const char * output;
+    int duplicate = 0;
+    int W_FLAGS = O_CREAT | O_WRONLY | O_TRUNC;
+    int C_PERMS = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH ;
 
-  if (argc == 3)
-  {
-    output = "fconc.out";
-  }
-  else if (argc == 4)
-  {
-    output = argv[3];
-  }
-  else
-  {
-    perror("Usage: ./fconc infile1 infile2 [outfile (default:fconc.out)]\n");
-    exit(EX_USAGE);
-  }
-
-  for (i=1; i<3; i++)
-  {
-    if (strcmp (argv[i], output) == 0)
+    if (argc == 3)
     {
-      duplicate = 1;
-      break;
+        output = "fconc.out";
     }
-  }
-
-  if (duplicate)               //if outfile matches an infile, work on a tempfile
-  {
-    TMP = open("/tmp/fconc.out.tmp",W_FLAGS,C_PERMS);
-    if (TMP < 0)
+    else if (argc == 4)
     {
-      perror("Error opening tmp file, is another instance running?\n");
-      exit(EX_TEMPFAIL);
+        output = argv[3];
+    }
+    else
+    {
+        perror("Usage: ./fconc infile1 infile2 [outfile (default:fconc.out)]\n");
+        exit(EX_USAGE);
     }
 
-    fcntl(TMP,F_GETLK,lock);  //get lock info on fd
-    lock.l_type = F_WRLCK;    //set lock to write lock
-    fcntl(TMP,F_SETLK,lock);  //set the lock on fd
-    write_file(TMP,argv[1]);  //write on fd
-    write_file(TMP,argv[2]);
-    lock.l_type = F_UNLCK;    //set lock to unlock
-    fcntl(TMP,F_SETLK,lock);  //set the lock on fd
-    close(TMP);               //close fd
-    OUT = open(output,W_FLAGS,C_PERMS);
-
-    if (OUT < 0)
+    for (i=1; i<3; i++)
     {
-      perror("Error handling output file\n");
-      exit(EX_IOERR);
+        if (strcmp (argv[i], output) == 0)
+        {
+            duplicate = 1;
+            break;
+        }
     }
-    fcntl(OUT,F_GETLK,lock);
-    lock.l_type = F_WRLCK;
-    fcntl(OUT,F_SETLK,lock);
 
-    write_file(OUT,"/tmp/fconc.out.tmp");
-    lock.l_type = F_UNLCK;
-    fcntl(OUT,F_SETLK,lock);
-    close (OUT);
-    if (unlink("/tmp/fconc.out.tmp") != 0)
+    if (duplicate)               //if outfile matches an infile, work on a tempfile
     {
-      perror("Error deleting temporary file, please remove /tmp/fconc.out.tmp\n");
-      exit(EX__BASE);
-    }
-  }
+        TMP = open("/tmp/fconc.out.tmp",W_FLAGS,C_PERMS);
+        if (TMP < 0)
+        {
+            perror("Error opening tmp file");
+            exit(EX_TEMPFAIL);
+        }
 
-  else
-  {
-    OUT = open(output,W_FLAGS,C_PERMS);
-    if (OUT < 0)
+        lock_file(TMP,F_WRLCK);
+        write_file(TMP,argv[1]);  //write on fd
+        write_file(TMP,argv[2]);
+        unlock_file(TMP);
+        if ( close(TMP) < 0)
+        {
+            perror("Could  not close tmp file");
+            exit(EX_IOERR);
+        }
+        OUT = open(output,W_FLAGS,C_PERMS);
+        if (OUT < 0)
+        {
+            perror("Error handling output file\n");
+            exit(EX_IOERR);
+        }
+
+        lock_file(OUT,F_WRLCK);
+        write_file(OUT,"/tmp/fconc.out.tmp");
+        unlock_file(OUT);
+        if ( close (OUT) < 0 )
+        {
+            perror("Could not close output file");
+            exit(EX_IOERR);
+        }
+        if (unlink("/tmp/fconc.out.tmp") != 0)
+        {
+            perror("Error deleting temporary file, please remove /tmp/fconc.out.tmp\n");
+            exit(EX__BASE);
+        }
+    }
+
+    else
     {
-      perror("Error handling output file\n");
-      exit(EX_IOERR);
+        OUT = open(output,W_FLAGS,C_PERMS);
+        if (OUT < 0)
+        {
+            perror("Error handling output file\n");
+            exit(EX_IOERR);
+        }
+        lock_file(OUT,F_WRLCK);
+        write_file(OUT,argv[1]);
+        write_file(OUT,argv[2]);
+        unlock_file(OUT);
+        if ( close(OUT) < 0 )
+        {
+            perror("Could not close out file");
+            exit(EX_IOERR);
+        }
     }
-    fcntl(OUT,F_GETLK,lock);
-    lock.l_type = F_WRLCK;
-    fcntl(OUT,F_SETLK,lock);
 
-    write_file(OUT,argv[1]);
-    write_file(OUT,argv[2]);
-
-    lock.l_type = F_UNLCK;
-    fcntl(OUT,F_SETLK,lock);
-    close(OUT);
-  }
-
-  exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
 
 void doWrite(int fd,const char *buff,int len)
 {
-  int written = 0;
-  int current = 0;
-  do
-  {
-    if ( (current = write(fd,buff+written,len-written)) < 0 )
+    int written = 0;
+    int current = 0;
+    do
     {
-      perror("Error in writing\n");
-      exit(EX_IOERR);
-    }
-    written+=current;
-  } while(written < len );
+        if ( (current = write(fd,buff+written,len-written)) < 0 )
+        {
+            perror("Error in writing\n");
+            exit(EX_IOERR);
+        }
+        written+=current;
+    } while(written < len );
 }
 
 
 void write_file(int fd,const char *infile)
 {
-  int A;
-  char buffer[BUFFER_SIZE];
-  int chars_read=0;
-  struct flock lock;
-  A = open(infile,O_RDONLY);
-  if (A ==-1)
-  {
-    char error_message[BUFFER_SIZE];
-    sprintf(error_message,"%s",infile);
-    perror(error_message);
-    exit(EX_NOINPUT);
-  }
-  fcntl(A,F_GETLK,lock);  //get lock info on A
-  lock.l_type = F_RDLCK;  //set lock to read lock
-  fcntl(A,F_SETLK,lock);  //set lock on A
-  //time to read
-  while( (chars_read = read(A,buffer,BUFFER_SIZE)) > 0)
-  {
-    //and write
-    doWrite(fd,buffer,chars_read);
-  }
-  if ( chars_read == -1 )
-  {
-    perror("Read Error\n");
-    exit(EX_IOERR);
-  }
-  lock.l_type = F_UNLCK;  //set lock to unlock
-  fcntl(A,F_SETLK,lock);  //set lock on A
-  //ok close
-  if ( close(A) == - 1 )
-  {
-    perror("Close Error\n");
-    exit(EX_IOERR);
-  }
+    int A;
+    char buffer[BUFFER_SIZE];
+    int chars_read=0;
+    A = open(infile,O_RDONLY);
+    if (A ==-1)
+    {
+        char error_message[BUFFER_SIZE];
+        snprintf(error_message,BUFFER_SIZE,"%s",infile);
+        perror(error_message);
+        exit(EX_NOINPUT);
+    }
+    lock_file(A,F_RDLCK);
+    while( (chars_read = read(A,buffer,BUFFER_SIZE)) > 0)
+    {
+        //and write
+        doWrite(fd,buffer,chars_read);
+    }
+    if ( chars_read == -1 )
+    {
+        perror("Read Error\n");
+        exit(EX_IOERR);
+    }
+    unlock_file(A);
+    if ( close(A) == - 1 )
+    {
+        perror("Close Error\n");
+        exit(EX_IOERR);
+    }
 }
 
+void lock_file(int fd,int lock_type)
+{
+    struct flock lock;
+    lock.l_type = lock_type;  //set lock to lock_type
+    lock.l_whence = 0;
+    lock.l_start = 0;
+    lock.l_len = 0;
+    if ( fcntl(fd,F_SETLKW,&lock) == -1)
+    {
+        if ( errno == EINTR )
+        {
+            perror("Interrupted before being able to set lock");
+            exit(EX_IOERR);
+        }
+        else if ( errno == EDEADLK )
+        {
+            perror("DEADLOCK met");
+            exit(EX_IOERR);
+        }
+        else
+        {
+            perror("Error while trying to set lock on file");
+            exit(EX_IOERR);
+        }
+    }
+}
+
+void unlock_file(int fd)
+{
+    lock_file(fd,F_UNLCK); //set lock on file to UNLOCK
+}
--- ask_1/stage_1/fconc.h
+++ ask_1/stage_1/fconc.h
@@ -2,7 +2,7 @@
 
 * File Name : fconc.h
 
-* Last Modified : Thu 17 Nov 2011 10:07:16 PM EET
+* Last Modified : Thu 24 Nov 2011 03:36:19 PM EET
 
 * Created By : Greg Liras <gregliras@gmail.com>
 
@@ -19,6 +19,8 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/errno.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sysexits.h>
@@ -26,4 +28,6 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 
 void doWrite(int fd, const char *buff, int len);
 void write_file(int fd, const char *infile);
+void lock_file(int fd,int lock_type);
+void unlock_file(int fd);
 #endif //FCONC_H
